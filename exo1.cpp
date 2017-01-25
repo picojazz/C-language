6d81a3090b8e75571ebe2d17ea5b7f9219110f5d
picojazz <picojazzz@gmail.com>
2017-01-25 13:06:14 +0000

    somme et produit de 3 nombre

---
+++ exo1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	int a,b,c;
+	cout << " entrez les valeurs" <<endl;
+	cin >> a >> b >> c ;
+	cout << "somme : " << a+b+c << endl;
+	cout << "produit :" << a*b*c << endl;
+	getch();
+	return 0;
+}
